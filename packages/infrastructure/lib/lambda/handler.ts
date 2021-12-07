032056fe8842aaff0cb0a730ac9ccfb344f27012
Giedrius Kaskonas <pkpersonal@protonmail.com>
2021-12-07 15:37:10 +0000

    fix lambda

--- packages/infrastructure/lib/lambda/handler.ts
+++ packages/infrastructure/lib/lambda/handler.ts
@@ -1,67 +1,72 @@
-import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import { DynamoDB } from "aws-sdk"
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
 
 let statusCode = 200;
 const headers = {
-    "Content-Type": "application/json"
+  "Content-Type": "application/json",
 };
-export async function handler(event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> {
+export async function handler(
+  event: APIGatewayEvent,
+  _context: Context
+): Promise<APIGatewayProxyResult> {
+  console.log(event);
 
-    console.log(event)
+  const dynamoClient = new DynamoDB.DocumentClient();
 
+  const tableName = process.env.TABLE_ID ?? "default";
 
-    const dynamoClient = new DynamoDB.DocumentClient()
+  let body;
 
-    const tableName = process.env.TABLE_ID ?? "default"
+  switch (event.httpMethod) {
+    case "GET":
+      if (event.resource == "/items") {
+        body = await dynamoClient
+          .scan({
+            TableName: tableName,
+          })
+          .promise();
 
-    let body;
+        body = JSON.stringify(body.Items);
+        break;
+      }
 
-    switch (event.httpMethod) {
-        case "GET":
-            if (event.resource == "/items") {
-                body = await dynamoClient.scan({
-                    TableName: tableName
-                }).promise()
+      if (!event.pathParameters) throw new Error("No path");
+      if (!event.pathParameters.item) throw new Error("No path");
 
-                body = JSON.stringify(body.Items);
-                break;
-            }
+      console.log(event.pathParameters.item);
+      body = await dynamoClient
+        .get({
+          TableName: tableName,
+          Key: {
+            id: parseInt(event.pathParameters.item),
+          },
+        })
+        .promise();
 
-            if (!event.pathParameters) throw new Error("No path")
-            if (!event.pathParameters.item) throw new Error("No path")
+      body = JSON.stringify(body);
+      break;
+    case "POST":
+      if (!event.body) throw new Error("No body!");
+      let requestJSON = JSON.parse(event.body);
+      await dynamoClient
+        .put({
+          TableName: tableName,
+          Item: {
+            id: requestJSON.id,
+            price: requestJSON.price,
+            name: requestJSON.name,
+          },
+        })
+        .promise();
+      body = `Put item ${requestJSON.id}`;
+      break;
+    default:
+      throw new Error(`Unsupported route:"`);
+  }
 
-            console.log(event.pathParameters.item)
-            body = await dynamoClient.get({
-                TableName: tableName,
-                Key: {
-                    id: parseInt(event.pathParameters.item)
-                }
-            }).promise()
-
-            body = JSON.stringify(body);
-            break;
-        case "POST":
-            if (!event.body) throw new Error("No body!")
-            let requestJSON = JSON.parse(event.body);
-            await dynamoClient
-                .put({
-                    TableName: tableName,
-                    Item: {
-                        id: requestJSON.id,
-                        price: requestJSON.price,
-                        name: requestJSON.name
-                    }
-                })
-                .promise();
-            body = `Put item ${requestJSON.id}`;
-            break;
-        default:
-            throw new Error(`Unsupported route:"`);
-    }
-
-    return {
-        statusCode,
-        body,
-        headers
-    };
+  return {
+    statusCode,
+    body,
+    headers,
+  };
 }

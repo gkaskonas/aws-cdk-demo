8605c0e2549f64f191bedf690b3363d75b58b68e
Giedrius Kaskonas <pkpersonal@protonmail.com>
2021-12-08 08:22:06 +0000

    migrate to CloudFrontWebDistribution

--- packages/infrastructure/lib/stacks/website.ts
+++ packages/infrastructure/lib/stacks/website.ts
@@ -1,9 +1,16 @@
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketEncryption,
+} from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
-import { Distribution } from "aws-cdk-lib/aws-cloudfront";
-import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import {
+  CloudFrontWebDistribution,
+  OriginAccessIdentity,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
 
 /**
  * A stack for our simple Lambda-powered web service
@@ -17,17 +24,29 @@ export class WebsiteStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const oai = new OriginAccessIdentity(this, "oai");
+
     const bucket = new Bucket(this, "websiteBucket", {
       encryption: BucketEncryption.S3_MANAGED,
       enforceSSL: true,
-      websiteIndexDocument: "index.html",
-      publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
     });
 
-    const distro = new Distribution(this, "distro", {
-      defaultBehavior: {
-        origin: new S3Origin(bucket),
-      },
+    const distro = new CloudFrontWebDistribution(this, "distro", {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket,
+            originAccessIdentity: oai,
+          },
+          behaviors: [
+            {
+              isDefaultBehavior: true,
+              viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            },
+          ],
+        },
+      ],
     });
 
     new BucketDeployment(this, "bucketDeployment", {
@@ -37,7 +56,7 @@ export class WebsiteStack extends Stack {
     });
 
     this.urlOutput = new CfnOutput(this, "Url", {
-      value: bucket.bucketWebsiteUrl,
+      value: distro.distributionDomainName,
     });
   }
 }

98804a5e10d1b25a757892f0a346d72e2c7ca956
gKaskonas <pkpersonal@protonmail.com>
2021-12-29 16:57:33 +0000

    fix dev cert

--- packages/infrastructure/lib/utils/environments.ts
+++ packages/infrastructure/lib/utils/environments.ts
@@ -7,6 +7,6 @@ export enum TargetRegions {
 }
 
 export enum CloudFrontCertificates {
-  DEV = "arn:aws:acm:eu-west-1:404319983256:certificate/0a515a9c-abfd-44b3-82f8-8c20d0b84d50",
+  DEV = "arn:aws:acm:us-east-1:404319983256:certificate/118f8e62-07b0-490b-aba5-35c7916b3f68",
   PROD = "arn:aws:acm:us-east-1:119184259962:certificate/f0dca187-ff36-46bf-a19d-d9bbffc3d392"
 }

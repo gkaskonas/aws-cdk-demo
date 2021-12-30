6ad755165bf783539e0649d5bf3bd6cb3cabbece
gKaskonas <pkpersonal@protonmail.com>
2021-12-30 20:34:29 +0000

    Update submit form

--- packages/application/src/components/ContactUs.js
+++ packages/application/src/components/ContactUs.js
@@ -9,7 +9,6 @@ export default class ContactUs extends Component {
     }
   }
   render() {
-    let resumeData = this.props.resumeData;
     return (
       <section id="contact">
           <div className="row section-head">
@@ -19,26 +18,18 @@ export default class ContactUs extends Component {
               </p>
             </div>
           </div>
-          <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-                <a href = {"mailto:" + resumeData.email}>Email: {resumeData.email}
-                </a>
-              </div>
-            </aside>
-          </div>
           <div>        
             <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
           <div className="form-group">
-            <label htmlFor="name"></label>
+            <label htmlFor="name">Name</label>
             <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
           </div>
           <div className="form-group">
-            <label htmlFor="exampleInputEmail1"></label>
+            <label htmlFor="exampleInputEmail1">Email</label>
             <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
           </div>
           <div className="form-group">
-            <label htmlFor="message"></label>
+            <label htmlFor="message">Message</label>
             <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>

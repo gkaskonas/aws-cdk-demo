d87ca5116c660dadf308693c6676cf58df7b056e
gKaskonas <pkpersonal@protonmail.com>
2021-12-30 11:36:17 +0000

    add contact form

---
+++ packages/infrastructure/lib/stacks/contact-form.ts
@@ -0,0 +1,60 @@
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
+import {
+  Cors,
+  LambdaIntegration,
+  LambdaRestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import { TargetRegions } from "../utils/environments";
+
+/**
+ * A stack for our simple Lambda-powered web service
+ */
+export class ContactFormStack extends Stack {
+  /**
+   * The URL of the API Gateway endpoint, for use in the integ tests
+   */
+  public readonly urlOutput: CfnOutput;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The Lambda function that contains the functionality
+    const handler = new Function(this, "Lambda", {
+      runtime: Runtime.NODEJS_14_X,
+      handler: "handler.handle",
+      code: Code.fromAsset(path.resolve(__dirname, "../lambda/contact/")),
+      environment: {
+        SES_REGION: TargetRegions.EUROPE,
+        FROM_NAME: "Peter Kaskonas",
+        FROM_EMAIL: "contact@peterkaskonas.com"
+      },
+      logRetention: RetentionDays.TWO_WEEKS,
+    });
+
+
+    // An API Gateway to make the Lambda web-accessible
+    const api = new LambdaRestApi(this, id, {
+      restApiName: `contact-form-api`,
+      handler,
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: Cors.DEFAULT_HEADERS,
+      },
+      proxy: false,
+    });
+
+    const contactFormIntegration = new LambdaIntegration(handler);
+
+    const items = api.root.addResource("submit");
+    items.addMethod("POST", contactFormIntegration); // POST /items
+
+    this.urlOutput = new CfnOutput(this, "Url", {
+      value: api.url,
+    });
+  }
+}
--- packages/infrastructure/lib/stages/websiteStage.ts
+++ packages/infrastructure/lib/stages/websiteStage.ts
@@ -1,5 +1,6 @@
 import { CfnOutput, Stage, StageProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { ContactFormStack } from "../stacks/contact-form";
 import { WebsiteStack } from "../stacks/website";
 
 /**
@@ -7,13 +8,16 @@ import { WebsiteStack } from "../stacks/website";
  */
 export class WebsiteStage extends Stage {
   public readonly urlOutput: CfnOutput;
+  public readonly contactFormUrl: CfnOutput;
 
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
 
     const website = new WebsiteStack(this, "Website");
+    const contactForm = new ContactFormStack(this, "contactForm");
 
     // Expose CdkpipelinesDemoStack's output one level higher
     this.urlOutput = website.urlOutput;
+    this.contactFormUrl = contactForm.urlOutput;
   }
 }

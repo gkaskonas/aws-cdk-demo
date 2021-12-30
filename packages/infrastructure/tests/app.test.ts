c9fb74497756fbfa6bc507b4f55ef2793bc87231
gKaskonas <pkpersonal@protonmail.com>
2021-12-30 15:09:32 +0000

    remove appStack

--- packages/infrastructure/tests/app.test.ts
+++ packages/infrastructure/tests/app.test.ts
@@ -1,29 +1,6 @@
 import { Template } from "aws-cdk-lib/assertions";
 import { App } from "aws-cdk-lib";
-import { ApplicationStack } from "../lib/stacks/application";
 import { WebsiteStack } from "../lib/stacks/website";
-
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-cdk-demo-stack.ts
-test("DynamoDB Created", () => {
-  const app = new App();
-  // WHEN
-  const stack = new ApplicationStack(app, "MyTestStack");
-  // THEN
-  const template = Template.fromStack(stack);
-  template.hasResourceProperties("AWS::DynamoDB::Table", {
-    PointInTimeRecoverySpecification: {
-      PointInTimeRecoveryEnabled: true,
-    },
-    SSESpecification: {
-      SSEEnabled: true,
-    },
-    ProvisionedThroughput: {
-      ReadCapacityUnits: 1,
-      WriteCapacityUnits: 1,
-    },
-  });
-});
 
 test("S3 Bucket Created", () => {
   const app = new App();

f0b64fabbbb980f7d8baf2be9d84456b1a100efe
gKaskonas <pkpersonal@protonmail.com>
2021-12-30 14:58:23 +0000

    update handler

--- packages/infrastructure/lib/lambda/contact/handler.ts
+++ packages/infrastructure/lib/lambda/contact/handler.ts
@@ -1,73 +1,109 @@
-import { APIGatewayProxyEvent } from "aws-lambda"
+import {APIGatewayProxyEventV2, APIGatewayProxyResultV2} from 'aws-lambda';
+import AWS from 'aws-sdk';
 
-import aws from 'aws-sdk'
-import { SendEmailRequest } from "aws-sdk/clients/sesv2"
-const ses = new aws.SESV2()
-const myEmail = process.env.EMAIL
-const myDomain = process.env.DOMAIN
+export type ContactDetails = {
+  name: string;
+  email: string;
+  message: string;
+};
 
-function generateResponse (code: number, payload: any) {
-  return {
-    statusCode: code,
-    headers: {
-      'Access-Control-Allow-Origin': myDomain,
-      'Access-Control-Allow-Headers': 'x-requested-with',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify(payload)
+export async function main(
+  event: APIGatewayProxyEventV2,
+): Promise<APIGatewayProxyResultV2> {
+  try {
+    if (!event.body)
+      throw new Error('Properties name, email and message are required.');
+
+    const {name, email, message} = JSON.parse(event.body) as ContactDetails;
+    if (!name || !email || !message)
+      throw new Error('Properties name, email and message are required');
+
+    return await sendEmail({name, email, message});
+  } catch (error: unknown) {
+    console.log('ERROR is: ', error);
+    if (error instanceof Error) {
+      return JSON.stringify({body: {error: error.message}, statusCode: 400});
+    }
+    return JSON.stringify({
+      body: {error: JSON.stringify(error)},
+      statusCode: 400,
+    });
   }
 }
 
-function generateError (code: number, err: any) {
-  console.log(err)
-  return {
-    statusCode: code,
-    headers: {
-      'Access-Control-Allow-Origin': myDomain,
-      'Access-Control-Allow-Headers': 'x-requested-with',
-      'Access-Control-Allow-Credentials': true
-    },
-    body: JSON.stringify(err.message)
+async function sendEmail({
+  name,
+  email,
+  message,
+}: ContactDetails): Promise<APIGatewayProxyResultV2> {
+  const SES_REGION  = process.env.REGION 
+  if (!SES_REGION) {
+    throw new Error(
+      'Please add the SES_EMAIL_TO, SES_EMAIL_FROM and SES_REGION environment variables in an env.js file located in the root directory',
+    );
   }
+  const ses = new AWS.SES({region: SES_REGION});
+  await ses.sendEmail(sendEmailParams({name, email, message})).promise();
+
+  return JSON.stringify({
+    body: {message: 'Email sent successfully 🎉🎉🎉'},
+    statusCode: 200,
+  });
 }
 
-function generateEmailParams (body: string): SendEmailRequest {
-  const { email, name, content } = JSON.parse(body)
-  console.log(email, name, content)
-  if (!(email && name && content)) {
-    throw new Error('Missing parameters! Make sure to add parameters \'email\', \'name\', \'content\'.')
+function sendEmailParams({name, email, message}: ContactDetails) {
+  const EMAIL  = process.env.EMAIL 
+  if (!EMAIL) {
+    throw new Error(
+      'Please add the SES_EMAIL_TO, SES_EMAIL_FROM and SES_REGION environment variables in an env.js file located in the root directory',
+    );
   }
-
-  if (!myEmail) throw new Error("Missing myEmail!")
-
   return {
-    FromEmailAddress: myEmail,
-    Destination: { ToAddresses: [myEmail] },
-    ReplyToAddresses: [email],
-    Content: {
-      Simple: {
+    Destination: {
+      ToAddresses: [EMAIL],
+    },
+    Message: {
       Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: getHtmlContent({name, email, message}),
+        },
         Text: {
           Charset: 'UTF-8',
-          Data: `Message sent from email ${email} by ${name} \nContent: ${content}`
-        }
+          Data: getTextContent({name, email, message}),
+        },
       },
       Subject: {
         Charset: 'UTF-8',
-        Data: `You received a message from ${myDomain}!`
-      }
-    }
-  }
-  }
+        Data: `Email from example ses app.`,
+      },
+    },
+    Source: EMAIL,
+  };
 }
 
-module.exports.send = async (event: APIGatewayProxyEvent) => {
-  try {
-    if (!event.body) throw new Error("No body!")
-    const emailParams = generateEmailParams(event.body)
-    const data = await ses.sendEmail(emailParams).promise()
-    return generateResponse(200, data)
-  } catch (err) {
-    return generateError(500, err)
-  }
+function getHtmlContent({name, email, message}: ContactDetails) {
+  return `
+    <html>
+      <body>
+        <h1>Received an Email. 📬</h1>
+        <h2>Sent from: </h2>
+        <ul>
+          <li style="font-size:18px">👤 <b>${name}</b></li>
+          <li style="font-size:18px">✉️ <b>${email}</b></li>
+        </ul>
+        <p style="font-size:18px">${message}</p>
+      </body>
+    </html> 
+  `;
+}
+
+function getTextContent({name, email, message}: ContactDetails) {
+  return `
+    Received an Email. 📬
+    Sent from:
+        👤 ${name}
+        ✉️ ${email}
+    ${message}
+  `;
 }

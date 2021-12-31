0d320e90fae8adc476b0c2374a06b311b7bd71b9
gKaskonas <pkpersonal@protonmail.com>
2021-12-31 17:01:02 +0000

    use matomo

--- packages/application/src/index.js
+++ packages/application/src/index.js
@@ -4,26 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
-
-const instance = createInstance({
-  urlBase: 'https://analytics.peterkaskonas.com',
-  siteId: 1,
-  trackerUrl: 'https://analytics.peterkaskonas.com/matomo.php', // optional, default value: `${urlBase}matomo.php`
-  srcUrl: 'https://analytics.peterkaskonas.com/matomo.js', // optional, default value: `${urlBase}matomo.js`
-  disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
-  heartBeat: { // optional, enabled by default
-    active: true, // optional, default value: true
-    seconds: 10 // optional, default value: `15
-  },
-  linkTracking: false, // optional, default value: true
-})
-
-ReactDOM.render(<MatomoProvider value={instance}>
-    <App />
-  </MatomoProvider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
